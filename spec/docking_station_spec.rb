cd5910dd3176f1f4623fc3c8906a1444c1d3c652
Marion LV <marion.levisage@gmail.com>
2016-01-19 16:37:56 +0000

    Updated files up to challenge 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,13 @@
-require 'docking_station.rb'
+require 'docking_station'
 
 describe DockingStation do
+
+  subject(:DockingStation) {described_class.new}
+
   it {should respond_to(:release_bike)}
+
+  it "gets a Bike and expects the bike to be working" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
