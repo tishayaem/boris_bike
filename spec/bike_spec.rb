d56925c5f4fcb5211ad3448be0cc204f12774394
Marion LV <marion.levisage@gmail.com>
2016-01-19 15:32:08 +0000

    Added lib and spec folders (including respective files)

---
+++ spec/bike_spec.rb
@@ -0,0 +1,5 @@
+require 'bike.rb'
+
+describe Bike do
+  it {should respond_to(:working?)}
+end
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,5 @@
+require 'docking_station.rb'
+
+describe DockingStation do
+  it {should respond_to(:release_bike)}
+end
