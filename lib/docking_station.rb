add77bf7e515b67c0142825b0bfa6f03212e9f4f
Marion LV <marion.levisage@gmail.com>
2016-01-20 12:25:36 +0000

    Updates from challenges 11 and 12

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,8 +1,19 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bike
 
   def release_bike
-   Bike.new
+    fail 'No bikes available' unless @bike
+    @bike
   end
+
+  def dock(bike)
+    @bike = bike
+  end
+
+  def bike
+    @bike
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,7 +7,21 @@ describe DockingStation do
   it {should respond_to(:release_bike)}
 
   it "gets a Bike and expects the bike to be working" do
-    bike = subject.release_bike
-    expect(bike).to be_working
+    bike = Bike.new
+    expect(bike.working?).to be true
   end
+
+  it {should respond_to(:dock)}
+
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
+  describe '#release_bike' do
+    it "raises an error when there are not bikes available" do
+      expect { subject.release_bike }.to raise_error "No bikes available"
+    end
+end
 end
