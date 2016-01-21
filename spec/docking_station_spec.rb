28015a45b8a4e99c43c75660af793ccb560239bf
Marion LV <marion.levisage@gmail.com>
2016-01-21 08:16:23 +0000

    Updated boris_bike up to challenge 16

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -27,7 +27,7 @@ describe DockingStation do
 
   describe '#dock' do
     it "raises an error when the dock is full" do
-      20.times { subject.dock Bike.new }
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock Bike.new }
       expect { subject.dock Bike.new }.to raise_error "No space available"
     end
   end
